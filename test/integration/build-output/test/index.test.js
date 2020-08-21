eyasudev/next.js

commit 638cbb6e9ab65f924ce224734144af25fa9bc36f

Add browser polyfils for Node.js modules (webpack 5 backwards compat) (#16022)
This adds the following Node.js core polyfills only when the import is used:

- `path`
- `stream`
- `vm`
- `crypto`
- `buffer`

Fixes #15948

We'll have a separate issue about adding warnings for the usage of these modules in the browser, some polyfills like crypto are quite heavy and generally not needed for most applications (included accidentally through node_modules).

packages/next/build/webpack-config.ts
@@ -356,7 +356,16 @@ export default async function getBaseWebpackConfig(
       'next/router': 'next/dist/client/router.js',
       'next/config': 'next/dist/next-server/lib/runtime-config.js',
       'next/dynamic': 'next/dist/next-server/lib/dynamic.js',
-      next: NEXT_PROJECT_ROOT,
+      ...(isServer
+        ? {}
+        : {
+            stream: 'stream-browserify',
+            path: 'path-browserify',
+            crypto: 'crypto-browserify',
+            buffer: 'buffer',
+            vm: 'vm-browserify',
+            next: NEXT_PROJECT_ROOT,
+          }),
       [PAGES_DIR_ALIAS]: pagesDir,
       [DOT_NEXT_ALIAS]: distDir,
       ...getOptimizedAliases(isServer),

test/integration/basic/test/index.test.js
@@ -2,6 +2,7 @@
 
 import { join } from 'path'
 import { renderViaHTTP, findPort, launchApp, killApp } from 'next-test-utils'
+import webdriver from 'next-webdriver'
 
 // test suits
 import hmr from './hmr'
@@ -32,6 +33,24 @@ describe('Basic Features', () => {
     ])
   })
   afterAll(() => killApp(context.server))
+
+  it('should polyfill Node.js modules', async () => {
+    const browser = await webdriver(context.appPort, '/node-browser-polyfills')
+    await browser.waitForCondition('window.didRender')
+
+    const data = await browser
+      .waitForElementByCss('#node-browser-polyfills')
+      .text()
+    const parsedData = JSON.parse(data)
+
+    expect(parsedData.vm).toBe(105)
+    expect(parsedData.hash).toBe(
+      'b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9'
+    )
+    expect(parsedData.path).toBe('/hello/world/test.txt')
+    expect(parsedData.buffer).toBe('hello world')
+    expect(parsedData.stream).toBe(true)
+  })
 
   dynamic(context, (p, q) => renderViaHTTP(context.appPort, p, q))
   hmr(context, (p, q) => renderViaHTTP(context.appPort, p, q))

test/integration/production/test/index.test.js
@@ -70,6 +70,24 @@ describe('Production Usage', () => {
       })
     }
 
+    it('should polyfill Node.js modules', async () => {
+      const browser = await webdriver(appPort, '/node-browser-polyfills')
+      await browser.waitForCondition('window.didRender')
+
+      const data = await browser
+        .waitForElementByCss('#node-browser-polyfills')
+        .text()
+      const parsedData = JSON.parse(data)
+
+      expect(parsedData.vm).toBe(105)
+      expect(parsedData.hash).toBe(
+        'b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9'
+      )
+      expect(parsedData.path).toBe('/hello/world/test.txt')
+      expect(parsedData.buffer).toBe('hello world')
+      expect(parsedData.stream).toBe(true)
+    })
+
     it('should allow etag header support', async () => {
       const url = `http://localhost:${appPort}/`
       const etag = (await fetch(url)).headers.get('ETag')

test/integration/basic/pages/node-browser-polyfills.js
@@ -1,0 +1,48 @@
+import { Writable } from 'stream'
+import path from 'path'
+import crypto from 'crypto'
+import { Buffer } from 'buffer'
+import vm from 'vm'
+import { useEffect, useState } from 'react'
+
+export default function NodeBrowserPolyfillPage() {
+  const [state, setState] = useState({})
+  useEffect(() => {
+    let closedStream = false
+
+    const writable = new Writable({
+      write(_chunk, _encoding, callback) {
+        callback()
+      },
+    })
+
+    writable.on('finish', () => {
+      closedStream = true
+    })
+
+    writable.end()
+
+    setState({
+      path: path.join('/hello/world', 'test.txt'),
+      hash: crypto.createHash('sha256').update('hello world').digest('hex'),
+      buffer: Buffer.from('hello world').toString('utf8'),
+      vm: vm.runInNewContext('a + 5', { a: 100 }),
+      stream: closedStream,
+    })
+  }, [])
+
+  useEffect(() => {
+    if (state.vm) {
+      window.didRender = true
+    }
+  }, [state])
+
+  return (
+    <>
+      <div
+        id="node-browser-polyfills"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(state) }}
+      ></div>
+    </>
+  )
+}

test/integration/production/pages/node-browser-polyfills.js
@@ -1,0 +1,48 @@
+import { Writable } from 'stream'
+import path from 'path'
+import crypto from 'crypto'
+import { Buffer } from 'buffer'
+import vm from 'vm'
+import { useEffect, useState } from 'react'
+
+export default function NodeBrowserPolyfillPage() {
+  const [state, setState] = useState({})
+  useEffect(() => {
+    let closedStream = false
+
+    const writable = new Writable({
+      write(_chunk, _encoding, callback) {
+        callback()
+      },
+    })
+
+    writable.on('finish', () => {
+      closedStream = true
+    })
+
+    writable.end()
+
+    setState({
+      path: path.join('/hello/world', 'test.txt'),
+      hash: crypto.createHash('sha256').update('hello world').digest('hex'),
+      buffer: Buffer.from('hello world').toString('utf8'),
+      vm: vm.runInNewContext('a + 5', { a: 100 }),
+      stream: closedStream,
+    })
+  }, [])
+
+  useEffect(() => {
+    if (state.vm) {
+      window.didRender = true
+    }
+  }, [state])
+
+  return (
+    <>
+      <div
+        id="node-browser-polyfills"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(state) }}
+      ></div>
+    </>
+  )
+}

commit 4c6c08e0344762aa2e8fb46c13a6f61303a79e3e

Do not alias Node modules for webpack 4 (#16452)
Fixes #16259
Caused by #16022
Follow up issue: #16450

This PR skips polyfilling Node modules in webpack 4 which are ignored by the `browsers` key.

packages/next/build/webpack-config.ts
@@ -366,16 +366,16 @@ export default async function getBaseWebpackConfig(
       'next/router': 'next/dist/client/router.js',
       'next/config': 'next/dist/next-server/lib/runtime-config.js',
       'next/dynamic': 'next/dist/next-server/lib/dynamic.js',
-      ...(isServer
-        ? {}
-        : {
+      next: NEXT_PROJECT_ROOT,
+      ...(isWebpack5 && !isServer
+        ? {
             stream: require.resolve('stream-browserify'),
             path: require.resolve('path-browserify'),
             crypto: require.resolve('crypto-browserify'),
             buffer: require.resolve('buffer'),
             vm: require.resolve('vm-browserify'),
-            next: NEXT_PROJECT_ROOT,
-          }),
+          }
+        : undefined),
       [PAGES_DIR_ALIAS]: pagesDir,
       [DOT_NEXT_ALIAS]: distDir,
       ...getOptimizedAliases(isServer),

test/integration/build-output/fixtures/with-crypto/pages/index.js
@@ -1,0 +1,13 @@
+import { useEffect, useState } from 'react'
+import seedrandom from 'seedrandom'
+
+const rng = seedrandom('hello')
+
+export default function () {
+  const [value, setValue] = useState(null)
+  useEffect(() => {
+    if (value) return
+    setValue(rng())
+  }, [value])
+  return <div>{value == null ? 'loading' : value.toString()}</div>
+}

test/integration/build-output/test/index.test.js
@@ -134,6 +134,46 @@ describe('Build Output', () => {
     })
   })
 
+  describe('Crypto Application', () => {
+    let stdout
+    const appDir = join(fixturesDir, 'with-crypto')
+
+    beforeAll(async () => {
+      await remove(join(appDir, '.next'))
+    })
+
+    it('should not include crypto', async () => {
+      ;({ stdout } = await nextBuild(appDir, [], {
+        stdout: true,
+      }))
+
+      console.log(stdout)
+
+      const parsePageSize = (page) =>
+        stdout.match(
+          new RegExp(` ${page} .*?((?:\\d|\\.){1,} (?:\\w{1,})) `)
+        )[1]
+
+      const parsePageFirstLoad = (page) =>
+        stdout.match(
+          new RegExp(
+            ` ${page} .*?(?:(?:\\d|\\.){1,}) .*? ((?:\\d|\\.){1,} (?:\\w{1,}))`
+          )
+        )[1]
+
+      const indexSize = parsePageSize('/')
+      const indexFirstLoad = parsePageFirstLoad('/')
+
+      expect(parseFloat(indexSize)).toBeLessThanOrEqual(3)
+      expect(parseFloat(indexSize)).toBeGreaterThanOrEqual(2)
+      expect(indexSize.endsWith('kB')).toBe(true)
+
+      expect(parseFloat(indexFirstLoad)).toBeLessThanOrEqual(65)
+      expect(parseFloat(indexFirstLoad)).toBeGreaterThanOrEqual(60)
+      expect(indexFirstLoad.endsWith('kB')).toBe(true)
+    })
+  })
+
   describe('Custom App Output', () => {
     const appDir = join(fixturesDir, 'with-app')
 